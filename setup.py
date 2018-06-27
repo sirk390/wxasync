cf185d742a5979d21d6b791af3d66fba0d7996ce
Christian Bodt <christian.bodt@gmail.com>
2018-06-27 18:16:41 +0200

    -Add missing dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 setup(name='wxasync',
-      version='0.1',
+      version='0.2',
       description='asyncio for wxpython',
       url='http://github.com/sirk390/wxasync',
       author='C.Bodt',
       author_email='sirk390@gmail.com',
       license='MIT',
       package_dir={'': 'src'},
+      install_requires=[
+          'wxpython',
+      ],
       packages=[''],
       zip_safe=True)
