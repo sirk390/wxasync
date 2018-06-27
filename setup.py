69edee671fa0b76884121175af403e000d4494df
Christian Bodt <christian.bodt@gmail.com>
2018-06-27 17:11:24 +0200

    -Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='wxasync',
+      version='0.1',
+      description='asyncio for wxpython',
+      url='http://github.com/sirk390/wxasync',
+      author='C.Bodt',
+      author_email='sirk390@gmail.com',
+      license='MIT',
+      package_dir={'': 'src'},
+      packages=[''],
+      zip_safe=True)
