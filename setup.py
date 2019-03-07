3a1b57d4aeeac12923d75b6da319d6cef6c35a51
Christian Bodt <christian.bodt@gmail.com>
2019-03-07 13:23:13 +0100

    -Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wxasync',
-      version='0.3',
+      version='0.4',
       description='asyncio for wxpython',
       url='http://github.com/sirk390/wxasync',
       author='C.Bodt',
