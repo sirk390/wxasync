7255c14b19a153f286da588b1f0b4331ffe7c463
Christian Bodt <christian.bodt@gmail.com>
2022-09-18 19:52:13 +0200

    -Update version for fix of issue #22

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wxasync',
-      version='0.46',
+      version='0.47',
       description='asyncio for wxpython',
       url='http://github.com/sirk390/wxasync',
       author='C.Bodt',
